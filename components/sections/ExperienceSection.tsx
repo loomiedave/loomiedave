0b12e53a460354c49df9531a53b2590930d3d183
Benjamin <loomiedave@gmail.com>
2025-09-22 14:44:37 +0000

    Fixed: Removed Animated parent div preventing expandable card from popping into full view

--- components/sections/ExperienceSection.tsx
+++ components/sections/ExperienceSection.tsx
@@ -7,18 +7,12 @@ interface ProjectProps {
 const ExperienceSection = ({ isVisible }: ProjectProps) => {
   return (
     <section id="projects" className="p-6">
-      <div
-        className={`transition-all duration-700 ${
-          isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'
-        }`}
-      >
         <div className="mb-4">
           <div className="flex items-center mb-4 px-4">
             <h2 className="text-2xl font-light underline">Experience</h2>
           </div>
           <ExpandableCard cards={cards} />
         </div>
-      </div>
     </section>
   );
 };
